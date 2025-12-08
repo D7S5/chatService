19ddb363cc61a2ef497a2c925839e70d4d819ff7
austin <doublehoon2@gmail.com>
2025-12-08 11:12:14 +0900

    KST converter

--- src/main/java/com/example/chatservice/converter/OffsetDateTimeToLocalDateTimeConverter.java
+++ src/main/java/com/example/chatservice/converter/OffsetDateTimeToLocalDateTimeConverter.java
@@ -7,17 +7,19 @@ import java.time.LocalDateTime;
 import java.time.OffsetDateTime;
 import java.time.ZoneOffset;
 
-@Converter(autoApply = true)
-public class OffsetDateTimeToLocalDateTimeConverter implements AttributeConverter<OffsetDateTime, LocalDateTime> {
-    @Override
-    public LocalDateTime convertToDatabaseColumn(OffsetDateTime attribute) {
-        if (attribute == null) return null;
-        return LocalDateTime.ofInstant(attribute.toInstant(), ZoneOffset.UTC);
-    }
 
-    @Override
-    public OffsetDateTime convertToEntityAttribute(LocalDateTime dbData) {
-        if (dbData == null) return null;
-        return OffsetDateTime.of(dbData, ZoneOffset.UTC);
-    }
-}
+// DB 저장시 UTC Converter
+//@Converter(autoApply = true)
+//public class OffsetDateTimeToLocalDateTimeConverter implements AttributeConverter<OffsetDateTime, LocalDateTime> {
+//    @Override
+//    public LocalDateTime convertToDatabaseColumn(OffsetDateTime attribute) {
+//        if (attribute == null) return null;
+//        return LocalDateTime.ofInstant(attribute.toInstant(), ZoneOffset.UTC);
+//    }
+//
+//    @Override
+//    public OffsetDateTime convertToEntityAttribute(LocalDateTime dbData) {
+//        if (dbData == null) return null;
+//        return OffsetDateTime.of(dbData, ZoneOffset.UTC);
+//    }
+//}
