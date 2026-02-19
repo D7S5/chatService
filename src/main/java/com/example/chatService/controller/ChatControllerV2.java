76e9b52bfe9fa948a062ad589b9e6a8bf08b5b8e
austin <doublehoon2@gmail.com>
2026-02-19 14:44:28 +0900

    invite

--- src/main/java/com/example/chatService/controller/ChatControllerV2.java
+++ src/main/java/com/example/chatService/controller/ChatControllerV2.java
@@ -1,29 +1,21 @@
 package com.example.chatService.controller;
 
-import com.example.chatService.component.ChatRateLimiter;
-import com.example.chatService.dto.CreateRoomRequest;
-import com.example.chatService.dto.GroupMessageDto;
-import com.example.chatService.dto.RoomResponse;
-import com.example.chatService.dto.RoomType;
+import com.example.chatService.dto.*;
 import com.example.chatService.entity.ChatRoomV2;
-import com.example.chatService.kafka.GroupMessageProducer;
 import com.example.chatService.security.UserPrincipal;
 import com.example.chatService.service.ChatMessageService;
 import com.example.chatService.service.ChatRoomService;
 import com.example.chatService.service.RoomInviteService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
 import java.util.List;
-import java.util.Map;
 
 @RestController
 @Slf4j
@@ -32,10 +24,8 @@ import java.util.Map;
 public class ChatControllerV2 {
 
     private final ChatMessageService chatMessageService;
-    private final StringRedisTemplate redis;
     private final ChatRoomService chatRoomV2Service;
     private final RoomInviteService inviteService;
-
 
     @MessageMapping("/chat.send")
     public void send(GroupMessageDto msg,
@@ -58,14 +48,8 @@ public class ChatControllerV2 {
     @GetMapping("/invite/{token}")
     public ResponseEntity<?> enterByInvite(@PathVariable String token) {
 
-        String roomId = redis.opsForValue().get("room:invite:" + token);
-        if (roomId == null) {
-            return ResponseEntity.status(HttpStatus.GONE)
-                    .body("초대 링크가 만료되었습니다.");
-        }
-        return ResponseEntity.ok(
-                Map.of("roomId", roomId)
-        );
+        String roomId = inviteService.enterByInvite(token);
+        return ResponseEntity.ok(new InviteEnterResponse(roomId));
     }
 
     @GetMapping
